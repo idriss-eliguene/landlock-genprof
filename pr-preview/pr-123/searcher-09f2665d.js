13c7d43b1c303b383ab6ea4d96cf1cf111f5b434
idriss-eliguene <idriss-eliguene@users.noreply.github.com>
2026-08-19 21:48:15 +0000

    docs: preview for PR #123 — 714b0c082d99dc38365fea8e71d004b28ddbad0f 901c4541926c4edebbcea1f21ac607f8e803209a

--- pr-preview/pr-123/searcher-09f2665d.js
+++ pr-preview/pr-123/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-97830b50.js',
+                path_to_root + 'searchindex-a77a4a6f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
