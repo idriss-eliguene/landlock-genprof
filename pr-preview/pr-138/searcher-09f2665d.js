bbaa592b98286ebfc44aecf830530f26866855fa
idriss-eliguene <idriss-eliguene@users.noreply.github.com>
2026-08-19 13:35:28 +0000

    docs: preview for PR #138 — b2662b9648196aa438bd8e2949c47fd05103557e 2fd824a8488833bad8b047367caa511c4056e725

--- pr-preview/pr-138/searcher-09f2665d.js
+++ pr-preview/pr-138/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fea97777.js',
+                path_to_root + 'searchindex-127df19c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
