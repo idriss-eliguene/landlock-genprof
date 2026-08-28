61493952e4cc68d0be88bfc1b09d28a070d63b26
idriss-eliguene <idriss-eliguene@users.noreply.github.com>
2026-08-28 02:47:03 +0000

    docs: deploy master (dev) — 6903c529eeb45ac8a64919227db5c94b5aa1a0fd 6903c529eeb45ac8a64919227db5c94b5aa1a0fd

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5f0617c1.js',
+                path_to_root + 'searchindex-c8e56e68.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
